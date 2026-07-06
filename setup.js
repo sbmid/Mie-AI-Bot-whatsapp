978cc46123b7f4ed2980c1ff89fe50fad7493680
Azrial Galih Prasetyo <sebirumatahari@gmail.com>
2026-07-06 21:33:15 +0700

    add rm -rf node_modules before install

--- setup.js
+++ setup.js
@@ -13,6 +13,7 @@ try {
     execSync('git reset --hard origin/main', { stdio: 'inherit' });
 
     console.log("\n[2/2] Menginstal dependencies...");
+    try { execSync('rm -rf node_modules package-lock.json', { stdio: 'ignore' }); } catch (e) {}
     execSync('npm install --ignore-scripts', { stdio: 'inherit' });
 
     console.log("\n=========================================");
